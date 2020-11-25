c7ddbd0d346cb176caf9a107e4ca4827fe5ded49
Max Koren <Koren_Max@bah.com>
2020-11-25 15:42:42 -0500

    spaces and deleting

--- script.js
+++ script.js
@@ -3,45 +3,142 @@
 var signal;
 var timeout;
 var buffer = [];
+var invalid = false;
+var canDelete;
+
+var message = [];
 
 const DOT_THRESHOLD = 200;
 const CODE_THRESHOLD = 600;
 
-function on(event) {
-	if (event.code == 'Space' && !event.repeat) {
-		signal = Date.now();
-		window.clearTimeout(timeout);
-		lightOn(true);
+const SPACE_CHAR = ' ';
+
+function keyDown(event) {
+	if (!event.repeat) {
+		if (event.code == 'Space' && !event.shiftKey) {
+			signalOn()
+		} else if (inProgress()) {
+			if (event.code == 'Backspace') {
+				cancel();
+			}
+		} else {
+			switch (event.code) {
+				case 'Space': addSpace(); break;
+				case 'Backspace': deleteLast(); break;
+				case 'Enter': endLine(); break;
+				default:
+			}
+		}
+		if (invalid) {
+			clearTimeout(timeout);
+			clearInvalid();
+		}
 	}
 }
 
-function off(event) {
-	if (event.code == 'Space') {
-		let duration = Date.now() - signal;
-		let isDot = duration < DOT_THRESHOLD
-		buffer.push(isDot ? '.': '-');
-		displaySymbol(isDot);
-		timeout = window.setTimeout(endCode, CODE_THRESHOLD);
-		lightOn(false);
+function keyUp(event) {
+	if (event.code == 'Space' && signal) {
+		signalOff();
 	}
+}
+
+function signalOn() {
+	signal = Date.now();
+	clearTimeout(timeout);
+	lightOn(true);
+}
+
+function signalOff() {
+	let duration = Date.now() - signal;
+	console.log(duration);
+	let isDot = duration < DOT_THRESHOLD;
+	signal = 0;
+	buffer.push(isDot ? '.': '-');
+	displaySymbol(isDot);
+	timeout = setTimeout(endCode, CODE_THRESHOLD);
+	lightOn(false);
 }
 
 function endCode() {
 	let code = MORSE[buffer.join('')];
-	document.getElementById('text').innerText += code || '?';
-	// postData('http://catascopic.com', code);
-	clearSymbols();
 	buffer.length = 0;
+	if (code) {
+		send(code);
+		clearSymbols();
+	} else {
+		lightOn(false);
+		setInvalid(true);
+		timeout = setTimeout(clearInvalid, 1000);
+		canDelete = false;
+	}
+}
+
+function send(code) {
+	canDelete = true;
+	message.push(code);
+	display();
+}
+
+function cancel() {
+	signal = 0;
+	buffer.length = 0;
+	canDelete = false;
+	clearTimeout(timeout);
+	lightOn(false);
+	clearSymbols();
+}
+
+function addSpace() {
+	if (message.length && message[message.length - 1] != SPACE_CHAR) {
+		send(SPACE_CHAR);
+	}
+}
+
+function deleteLast() {
+	if (canDelete && message.length) {
+		message.pop();
+		display();
+		canDelete = false;
+	}
+}
+
+function endLine() {
+	if (!inProgress()) {
+		canDelete = false;
+		message.length = 0;
+		display();
+	}
+}
+
+function inProgress() {
+	return signal || buffer.length;
+}
+
+function display() {
+	document.getElementById('message').innerText = message.join('');
+}
+
+function clearInvalid() {
+	setInvalid(false);
+	clearSymbols();
 }
 
 function lightOn(state) {
 	document.getElementById('icon').classList.toggle('on', state);
 }
 
+function setInvalid(state) {
+	invalid = state;
+	document.getElementById('symbols').classList.toggle('invalid', state);
+}
+
+const DOT = new Image();
+const DASH = new Image();
+DOT.src = 'dot.svg';
+DASH.src = 'dash.svg';
+
 function displaySymbol(isDot) {
-	let img = document.createElement('img');
-	img.src = (isDot ? 'dot': 'dash') + '.svg';
-	document.getElementById('symbols').appendChild(img);
+	document.getElementById('symbols').appendChild((isDot ? DOT : DASH).cloneNode(false));
 }
 
 function clearSymbols() {
@@ -49,18 +146,6 @@ function clearSymbols() {
 	while (symbols.firstChild) {
 		symbols.lastChild.remove();
 	}
-}
-
-async function postData(url, data) {
-	let response = await fetch(url, {
-		method: 'POST',
-		cache: 'no-cache',
-		headers: { 
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(data)
-	});
-	return response.json();
 }
 
 const MORSE = {
@@ -100,5 +185,23 @@ const MORSE = {
 	"--...": "7",
 	"---..": "8",
 	"----.": "9",
-	"-----": "0"
+	"-----": "0",
+	
+	".-.-.-": ".",
+	"--..--": ",",
+	"..--..": "?",
+	".----.": "'",
+	"-.-.--": "!",
+	"-..-." : "(",
+	"-..-.-": ")",
+	".-..." : "&",
+	"---...": ":",
+	"-.-.-.": ";",
+	".-.-." : "+",
+	"-....-": "-",
+	"..--.-": "_",
+	".-..-.": "\"",
+	"...-..-": "$",
+	".--.-.": "@",
+	"-..-." : "/"
 };
